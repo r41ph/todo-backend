563848ba8ab3e08cdbd44ed2c2670c81705c0f92
r4lfl4b <dev@rgalan.es>
2019-11-10 17:31:38 +0000

    feat: add title to todo schema

--- todoSchema.js
+++ todoSchema.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 let Todo = new Schema(
   {
+    todo_title: { type: String },
     todo_description: { type: String },
     todo_completed: { type: Boolean }
   },
