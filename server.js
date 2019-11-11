eed983e94a26ea5b78f67b9089840b74e6980031
r4lfl4b <dev@rgalan.es>
2019-11-11 09:37:47 +0000

    feat: add update Todo end point

--- server.js
+++ server.js
@@ -49,6 +49,29 @@ todoRoutes.route("/").get(function(req, res) {
   });
 });
 
+// Update TODO
+todoRoutes.route("/update/:id").post(function(req, res) {
+  Todo.findById(req.params.id, function(err, todo) {
+    if (!todo) {
+      res.status(404).send("data is not found");
+    } else {
+      todo.todo_title = req.body.todo_title;
+      todo.todo_description = req.body.todo_description;
+      todo.todo_duedate = req.body.todo_duedate;
+      todo.todo_completed = req.body.todo_completed;
+    }
+
+    todo
+      .save()
+      .then(todo => {
+        res.json("Todo updated");
+      })
+      .catch(err => {
+        res.status(400).send("Update todo failed");
+      });
+  });
+});
+
 app.use("/api/todos/", todoRoutes);
 
 app.listen(PORT, function() {
